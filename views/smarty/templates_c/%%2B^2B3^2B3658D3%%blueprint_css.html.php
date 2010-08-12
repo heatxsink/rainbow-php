ce706977c2f7e9a1f9078cb0ecd4f24c70b8c777
Nicholas Granado <ngranado@gmail.com>
2010-08-12 00:41:31 -0700

    Some more directory cleanup.

--- views/smarty/templates_c/%%2B^2B3^2B3658D3%%blueprint_css.html.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-08-01 13:26:12
-         compiled from blueprint_css.html */ ?>
-
-<link rel="stylesheet" href="/static/css/blueprint/screen.css" type="text/css" media="screen, projection">
-<link rel="stylesheet" href="/static/css/blueprint/print.css" type="text/css" media="print">
-<!--[if lt IE 8]>
-<link rel="stylesheet" href="/static/css/blueprint/ie.css" type="text/css" media="screen, projection">
-<![endif]-->
