d629172b7bc3a059dd69cbbe02eef3b5661ab9bc
heatxsink <ngranado@gmail.com>
2010-10-22 19:14:21 -0700

    added a namespace function.php

--- lib/Rainbow/functions.php
+++ lib/Rainbow/functions.php
@@ -23,7 +23,7 @@
  *
  */
 
-function curl_http_get($url) {
+function rainbow_curl_http_get($url) {
 	$ssl = false;
 	$ch = curl_init($url);
 	$headers = array("User-Agent: rainbow-php");
@@ -40,14 +40,14 @@ function curl_http_get($url) {
 	return $response;
 }
 
-function curl_http_get_store($url, $filename) {
+function rainbow_curl_http_get_store($url, $filename) {
 	$data = curl_http_get($url);
 	$fh = fopen($filename, 'w');
 	fwrite($fh, $data);
 	fclose($fh);
 }
 
-function generate_guid() {
+function rainbow_generate_guid() {
 	// The field names refer to RFC 4122 section 4.1.2
 	return sprintf('%04x%04x%04x%03x4_%04x%04x%04x%04x',
 	mt_rand(0, 65535), mt_rand(0, 65535), // 32 bits for "time_low"
@@ -61,7 +61,7 @@ function generate_guid() {
 	);
 }
 
-function validate_var($var) {
+function rainbow_validate_var($var) {
 	$return_value = false;
 	if(isset($var) && strlen(trim($var)) > 0) {
 		$return_value = true;
@@ -69,7 +69,7 @@ function validate_var($var) {
 	return $return_value;
 }
 
-function validate_email_address($email) {
+function rainbow_validate_email_address($email) {
 	$qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
 	$dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
 	$atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c'.
@@ -87,20 +87,20 @@ function validate_email_address($email) {
 	return preg_match("!^$addr_spec$!", $email);
 }
 
-function get_client_ip_address() {
+function rainbow_get_client_ip_address() {
 	$ip_address = $_SERVER['REMOTE_ADDR'];
 	$ip_tokens = split('\.', $ip_address);
 	$ip_decimal = ($ip_tokens[3] + $ip_tokens[2] * 256 + $ip_tokens[1] * 256 * 256 + $ip_tokens[0] * 256 * 256 * 256);
 	return $ip_decimal;
 }
 
-function get_unix_timestamp() {
+function rainbow_get_unix_timestamp() {
 	$unix_time = time();
 	$gmt_mysql_time_string = gmdate("Y-m-d H:i:s", $unix_time);
 	return $gmt_mysql_time_string;
 }
 
-function json_encode_pretty($input) {
+function rainbow_json_encode_pretty($input) {
 	$tab = "  ";
 	$return_value = "";
 	$indent_level = 0;
